195007d9f71f3da47227c1361b48751ce320f881
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-07-23 10:00:09 +0530

    question populating

---
+++ Alexa.js
@@ -0,0 +1,36 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+const questions = require('./questions');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+
+const ANSWER_COUNT = 4;
+const GAME_LENGTH = 3;
+
+/* Randomly choosing game questions */
+
+function populateGameQuestions(translatedQuestions) {
+  const gameQuestions = [];
+  const indexList = [];
+  let index = translatedQuestions.length;
+  if (GAME_LENGTH > index) {
+    throw new Error('Invalid Game Length.');
+  }
+
+  for (let i = 0; i < translatedQuestions.length; i += 1) {
+    indexList.push(i);
+  }
+
+  for (let j = 0; j < GAME_LENGTH; j += 1) {
+    const rand = Math.floor(Math.random() * index);
+    index -= 1;
+
+    const temp = indexList[index];
+    indexList[index] = indexList[rand];
+    indexList[rand] = temp;
+    gameQuestions.push(indexList[index]);
+  }
+  return gameQuestions;
+}
