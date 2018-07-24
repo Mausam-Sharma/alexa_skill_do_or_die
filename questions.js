19bf8e057089a2cfecdc89f90531bbd8b87e945e
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-07-24 10:45:27 +0530

    question 1

---
+++ questions.js
@@ -0,0 +1,25 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  
+  QUESTIONS_EN_US: [
+    {
+      'you are driving your car, at a speed of 150 kilometres per hour, on a highway with mild traffic. suddenly, you noticed that gas of your car has been stuck, and breaks are having no effect, at such a high speed . now, in this situation, what would you do ? ': [
+        'Jam the car into neutral, and steer safely',
+        'Use emergency breaks ',
+        'Turn off the motor',
+        'Jump from the running vehicle',
+      ],
+    },
+    
+ ],
+   
+ 
+    
+    
+  
+};
