0e3faa4ab256a252de7a9ef3207ab997a84b9b6d
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-07-30 18:20:12 +0530

    is Answer slot valid

--- Alexa.js
+++ Alexa.js
@@ -81,5 +81,97 @@ function isAnswerSlotValid(intent) {
     && parseInt(intent.slots.Answer.value, 10) < (ANSWER_COUNT + 1)
     && parseInt(intent.slots.Answer.value, 10) > 0;
 }
+function handleUserGuess(userGaveUp, handlerInput) {
+  const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
+  const { intent } = requestEnvelope.request;
+
+  const answerSlotValid = isAnswerSlotValid(intent);
+
+  let speechOutput = '';
+  let speechOutputAnalysis = '';
+
+  const sessionAttributes = attributesManager.getSessionAttributes();
+  const gameQuestions = sessionAttributes.questions;
+  let correctAnswerIndex = parseInt(sessionAttributes.correctAnswerIndex, 10);
+  let currentScore = parseInt(sessionAttributes.score, 10);
+  let currentQuestionIndex = parseInt(sessionAttributes.currentQuestionIndex, 10);
+  const { correctAnswerText } = sessionAttributes;
+  const requestAttributes = attributesManager.getRequestAttributes();
+  const translatedQuestions = requestAttributes.t('QUESTIONS');
+
+
+  if (answerSlotValid
+    && parseInt(intent.slots.Answer.value, 10) === sessionAttributes.correctAnswerIndex) {
+    currentScore += 1;
+    speechOutputAnalysis = requestAttributes.t('ANSWER_CORRECT_MESSAGE');
+  } else {
+    if (!userGaveUp) {
+      speechOutputAnalysis = requestAttributes.t('ANSWER_WRONG_MESSAGE');
+    }
+
+    speechOutputAnalysis += requestAttributes.t(
+      'CORRECT_ANSWER_MESSAGE',
+      correctAnswerIndex,
+      correctAnswerText
+    );
+  }
+
+  // Check if we can exit the game session after GAME_LENGTH questions (zero-indexed)
+  if (sessionAttributes.currentQuestionIndex === GAME_LENGTH - 1) {
+    speechOutput = userGaveUp ? '' : requestAttributes.t('ANSWER_IS_MESSAGE');
+    speechOutput += speechOutputAnalysis + requestAttributes.t(
+      'GAME_OVER_MESSAGE',
+      currentScore.toString(),
+      GAME_LENGTH.toString()
+    );
+
+    return responseBuilder
+      .speak(speechOutput)
+      .getResponse();
+  }
+  currentQuestionIndex += 1;
+  correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
+  const spokenQuestion = Object.keys(translatedQuestions[gameQuestions[currentQuestionIndex]])[0];
+  const roundAnswers = populateRoundAnswers(
+    gameQuestions,
+    currentQuestionIndex,
+    correctAnswerIndex,
+    translatedQuestions
+  );
+  const questionIndexForSpeech = currentQuestionIndex + 1;
+  let repromptText = requestAttributes.t(
+    'TELL_QUESTION_MESSAGE',
+    questionIndexForSpeech.toString(),
+    spokenQuestion
+  );
+
+  for (let i = 0; i < ANSWER_COUNT; i += 1) {
+    repromptText += `${i + 1}. ${roundAnswers[i]}. `;
+  }
+
+  speechOutput += userGaveUp ? '' : requestAttributes.t('ANSWER_IS_MESSAGE');
+  speechOutput += speechOutputAnalysis
+    + requestAttributes.t('SCORE_IS_MESSAGE', currentScore.toString())
+    + repromptText;
+
+  const translatedQuestion = translatedQuestions[gameQuestions[currentQuestionIndex]];
+
+  Object.assign(sessionAttributes, {
+    speechOutput: repromptText,
+    repromptText,
+    currentQuestionIndex,
+    correctAnswerIndex: correctAnswerIndex + 1,
+    questions: gameQuestions,
+    score: currentScore,
+    correctAnswerText: translatedQuestion[Object.keys(translatedQuestion)[0]][0]
+  });
+
+  return responseBuilder.speak(speechOutput)
+    .reprompt(repromptText)
+    .withSimpleCard(requestAttributes.t('GAME_NAME'), repromptText)
+    .getResponse();
+}
+
+
 
 
