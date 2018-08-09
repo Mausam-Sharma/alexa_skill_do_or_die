140c7cb4e8a5a035b5063da9a8235c6818c05883
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-08-09 12:42:21 +0530

    english india translations

--- questions.js
+++ questions.js
@@ -233,6 +233,221 @@ module.exports = {
   ],
    
  
+  QUESTIONS_EN_IN: [
+    {
+      'You are driving your car, at a speed of 150 kilometres per hour, on a highway with mild traffic. Suddenly, you noticed that accelerator of your car has been stuck, and breaks are having no effect, at such a high speed . Now, in this situation, What would you do ? ': [
+        'Jam the car into neutral, and steer safely',
+        'Use emergency breaks',
+        'Turn off the engine',
+        'Jump from the running vehicle',
+      ],
+    },
+    {
+      'You are standing on a lagoon barrier, just 150 yards from the beach , the ocean suddenly rises , and a wave of a size of a two story building rises in, and is going to slam against the barrier, like a runaway train. , Killer wave is few seconds away , what would you do ? ': [
+        'take cover of a rock and hold on',
+        'Drop down and lie flat',     
+        'jump as the wave hits and, body surf to safety',
+        'try running away from the place',
+      ],
+    },
+    {
+      'you are working in your office, at your desk, all of a sudden, the building as well as the things around you, start shivering, yes that\'s an earthquake. Now, in this situation, What would you do ? ': [
+        'take cover under your desk furniture and, hold on till shaking stops',
+        'take shelter under doorway',
+        'rush outside office towards open space, away from building',
+        'run towards the terrace',
+      ],
+    },
+    {
+      'Your friend has participated , in a pastry eating competition. Suddenly , pastry got stuck in his food pipe, and even after drinking a lot of water , she is choking to death.  Now, in this situation, What would you do ? ': [
+        'begin abdominal thrusts',
+        'begin mouth-to-mouth resuscitation',
+        'hit on her back',
+        'call ambulance and wait for a doctor to do something',
+      ],
+    },
+    {
+      'You are cycling alone, on a mountaineous road , in a forest , suddenly ,a group of dogs, used to herd sheep, surrounds you and , they are trying to attack you. Now, in this situation, What would you do ? ': [
+        'Be Aggressive, shout loudly and make a lot of noise',
+        'Stay calm and ride away',
+        'Drop and Submit',
+        'fold your arms and stand idle ',
+       ],
+    },
+    {
+      'You are skiing on a mountain, suddenly you fall into a deadly trap, called tree well, which is a deep void around the base of a tree, concealed by loose snow. You somehow, managed to call emergency services with your phone , but now, you have to struggle until they rescue you. Now, in this situation, What would you do ? ': [
+        'use your hands to cover your mouth and wait for rescue team',
+        'twist your legs to create an opening',
+        'pull yourself closer to tree trunk and take shelter in tree well',
+        'remain idle in your position and start yelling for help',
+       ],
+    },
+
+    {
+      'You are on your regular, morning walk , suddenly , a stray dog starts barking on you ,and within a moment, he is raging towards you to attack . Now , What would you do ? ': [
+        'Drop and Submit and play dead, avoiding eye contact ',
+        'Raise your arm and look threatening',
+        'Jump on to a nearby car',
+        'shout and make a lot of noise',
+        'run away as fast as possible',
+       ],
+    },
+    {
+      'You are having fun with white water kayaking in a river, suddenly you got stuck in a devastating vortex, you along with your kayak is revolving, in so called natural washing machine, people around there, throw ropes towards you as lifelines. Now, in this situation, What would you do ? ': [
+        'curl into a ball and turtle up',
+        'Fight to keep your head above water',
+        'Try to grab a lifeline around your body',
+        'loose yourself into natural vortex flow',
+       ],
+    },
+    {
+      'Suppose, you are working in your backyard garden. Suddenly, a venomous snake, hidden there in the shrubs, attacks you on your hand, instantly delivering a potentially deadly dose of hemotoxic venom, now with uncontrollable bleeding, What would you do ? ': [
+        'carefully wash the wound with soap and water, call ambulance and stay calm until you get medical assistance',
+        'rush to find something to stop the flow of blood by making a tourniquet',
+        'cut the wound and suck out the poison',
+        'rush to the hospital to get medical assistance ',
+       ],
+    },
+    {
+      'You are on an african safari, in an open vehicle. Your vehicle comes inbetween a baby elephant and his overprotective mother. All of a sudden, this raging mother elephant slams into your vehicle and you are thrown out with your vehicle overturned. Now, in this situation, What would you do ? ': [
+        'go inside the vehicle and scare off the elephant, by making a lot of noise with your car horn',
+        'duck behind the overturned car',
+        'climb the nearest tree',
+        'run away as far as you can ',
+       ],
+    },
+    {
+      'you are riding a snowmobile, and you reach to an edge 400 feet above the valley, a massive block of snow suddenly breaks away, and now you are teetering on the edge of a four hundred foot drop to the rocks below. This is what is known as a cornice fall. Now, in this situation, What would you do ? ': [
+        'slowly get off the snowmobile and creep away',
+        'dive away from the edge as quickly as you can',
+        'ride in reverse and back off the edge',
+        'remain idle and wait for the help',
+       ],
+    },
+    {
+      'You are on a boat with your friends, in an ocean 50 miles away from shore, suddenly a marlin has made a run at your boat and jumped right over the railing. A marlin is a billfish that can launch itself up to 10 feets, and has a sword like bill. This ocean predator\'s powerful body is thrashing with enough force to take you out. Now, in this situation, What would you do ? ': [
+        'throw a towel over it\'s eyes and then push it back into water',
+        'sieze the tail and pull over the railing',
+        'grab the marlin by it\'s bill and push it back into the water',
+        'jump into the sea to save yourself from marlin',
+       ],
+    },
+    {
+      'You are in a zoo, and you see that a tourist crossed 2 safety barriers, to get a closer picture of a polar bear, and suddenly is now caught in the jaws of the bear. You instantly take some branches of a tree to take some action.  Now, in this situation, What would you do ? ': [
+        'beat the branches against the ground',
+        'pry open the bear\'s mouth with the branch',
+        'hit sensitive organs of bear with branches such as it\'s eyes',
+        'call the safety officials and wait for them to handle the case ',
+       ],
+    },
+    {
+      'You and your friends are flying home, after an afternoon exploring the wilderness. You are piloting the aircraft, suddenly, aircraft begins to lose power and is falling down, this is due to invisible killer, called high density altitutde, in which the air become dangerously thin. Now, in this situation, What would you do ? ': [
+        'open the door and cover your head',
+        'climb in the back seat',
+        'put your head down',
+        'jump from falling plane just a moment before it crashes to ground',
+       ],
+    },
+    {
+      'You are diving in the sea of Cortez, when a massive bull elephant seal, approaches and turns on you, Now, to escape from this underwater attack, What would you do ? ': [
+        'swat at the seal\'s proboscis',
+        'bang on your tank to scare the seal away',
+        'dive deep below the danger and wait it out',
+        'go towards the surface of sea',
+       ],
+    },
+    {
+      'You are in the wildlife rescue team, and you have been called to remove the most powerful snake in the world, from center of a small town, yes, that\'s an anaconda. You and 4 other members of team are handling the snake, suddenly, the snake grabs your hand, now you have few seconds before snake envelops you with it\'s deadly coils. Now, in this situation, What would you do ? ': [
+        'push your arm farther down the snake\'s throat away from curved teeth',
+        'try to hit the head of the snake with a heavy object',
+        'press your knee against the snake\'s throat to cut off it\'s air supply',
+        'drop onto it\'s back and try to pry open it\'s jaws from behind ',
+       ],
+    },
+    {
+      'you and your family are returning home from a trip in your car. On the highway, you see a giant wall of flames headed straight for your car on one side of the highway. Initially, you are unable to sense the danger, you keep on moving, but suddenly temperature rises,and you accelerate your car, but just powering through is no longer an option, you consider a u turn but it is too late, fire has surrounded you. Now, from this natural oven, how would you escape ? ': [
+        'pull over and turn on the air conditioner',
+        'drive through the flames to an area that is already burned out',
+        'go off road and and speed away from advancing fire',
+        'stop the car on highway and call emergency services to rescue you',
+       ],
+    },
+    {
+      'You are surfing the waves, suddenly you slip down from your surfing board, and now you have fallen in the white water, under the risk of being killed by a huge wave behind you. Now, in this situation, What would you do ? ': [
+        'take a breath and stay limber',
+        'swim with the wave',
+        'swim towards the light',
+        'try to find your surfing board and swim towards it',
+       ],
+    },
+    {
+      'You are driving your car in a thunderstorm, suddenly you observe a tornado coming towards you, you tried to flee away, but had taken a turn in wrong direction, and there is no road ahead. behind you is a terrible tornado with a wind speed of 195 kilometres per hour. Now, from this natural devastating blender, how would you save yourself ? ': [
+        'leave your car alone and lie flat on the ground away from car',
+        'take shelter under some overpass ',
+        'roll down windows and stay put',
+        'leave the car and start running away from tornado',
+       ],
+    },
+    {
+      'You are a professional mountain biker, riding your bike in the craggy canyon trails over utah. While riding, you lift off your bicycle for a dangerous jump above 80 metres high trench, but something goes wrong, and you are about to crash down on the surface with a speed of 80 miles per hour. Now, to minimise the damage, What would you do ? ': [
+        'bend your knees and try to roll over after crash',
+        'keep your hands on the handlebars and stay with your bike',
+        'land on your hands and feet',
+        'do nothing and let gravity do the work',
+       ],
+    },
+    {
+      'You are on a deck boat just near the shore, which has just swamped by another ship\'s wake, now it\'s taking on water at an alarming rate, and then suddenly, slams against a barge. Now, when your boat is about to sink, What would you do ? ': [
+        'Bail out and swim away from ship',
+        'move to the back of the seat and stay with it for as long as possible',
+        'gun the engine to move boat away from barge',
+        'wait for the rescue team to save you',
+       ],
+    },
+    {
+      'You are a base jumper, and you leap off a mile high mountain next to a lake, wearing specially designed wingsuit, 500 feet above the ground, you deploy your parachute, but parachute opens facing the wrong way and slams you into a solid rock wall, spiraling from the hit, your parachute lines twist and you swing back to the cliff. now, when you are few seconds away from death, What would you do ? ': [
+        'kick away from the cliff wall and aim for a soft landing',
+        'scissor kick to untwist the parachute lines',
+        'release the parachute and glide to lake',
+        'try to move closer to the cliff wall',
+       ],
+    },
+    {
+      'Anza Borrego desert state park is an off roading paradise in California. The park\'s most hazardous drop off, is a treacherous 60 degree slope, aptly called heart attack hill. Suppose you are driver of a 4 wheeler off road vehicle, you don\'t have a seat belt, and as you start descending the hill, your vehicle suddenly loses balance, and is on the verge of rolling. Now, in this situation, What would you do ? ': [
+        'brace your feet against the floorboards and lean towards the passenger seat',
+        'jump out of the jeep before it begins to roll',
+        'put the vehicle in reverse gear and try to move vehicle backwards up the hill',
+        'grip the wheel and steer away from the roll',
+       ],
+    },
+    {
+      'Suppose there is a train standing idle on the tracks, and you are trying to cross the tracks from under the standing train, all of a sudden, train starts moving, and you got stuck in the empty space between two rails of a track, with tons of lethal steel overhead. Train is moving at a speed of 11 miles per hour, and you have a 4 second window between the pass of each wheel. Now, trapped under this speeding train, What would you do? ': [
+        'lie flat, stay close to the ground, and try to be in the clearance between the train and the ground',
+        'time your escape and crawl out between the moving wheels',
+        'use your backpack to hook onto the train and pull yourself up',
+        'pray to god and hope for best',
+       ],
+    },
+    {
+      'You are at a petrol filling station. An old man is filling up his plastic can with petrol in the back of his pickup truck. The man is sticking the metal nozzle into the plastic can, suddenly the flowing petrol builds up a static charge, that ignites all the petrol vapors in the air, and a fire erupts, the man panics and throws the gas container covering himself in fuel, terrified and burning, he drops and rolls, but nothing happens. Now, being a bystander, What would you do ? ': [
+        'tell him to keep rolling and smoother the flames',
+        'call the fire fighters and wait for them to take any action',
+        'use your jacket to beat out the fire',
+        'dump gas station\'s window cleaner over the fire',
+       ],
+    },
+    {
+      'You are driving a truck on an icy road, road is made of asphalt on which ice has been accumulated, making the road smooth, slick and dangerous. Now, you apply your brakes while driving, but the truck keeps going, and skidding on the icy road. Now, to survive from this terrifying skid, What would you do ? ': [
+        'let off the gas and let your back wheels lead the way',
+        'watch for hazards ahead on the road and accelerate to re establish your grip on the road',
+        'apply slow constant pressure to the brakes',
+        'jump from the running truck',
+       ],
+    },
+    
+  ],
+   
+ 
     
     
   
